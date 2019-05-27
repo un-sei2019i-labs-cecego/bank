11ed98bee3873d70a9406217c9f5f938a211fc97
Gonzalo Diaz <egonzalodm@gmail.com>
2019-05-27 15:30:26 -0500

    Main

--- mobile-app/AppLogin/app/src/main/java/com/example/applogin/UI/MainActivity.java
+++ mobile-app/AppLogin/app/src/main/java/com/example/applogin/UI/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.applogin;
+package com.example.applogin.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.applogin.R;
 
 public class MainActivity extends AppCompatActivity {
 

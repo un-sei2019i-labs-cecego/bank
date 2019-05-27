328433df8d2f81c21067743eb9e05e2319b3fed7
Cerciber <ceatorresar@unal.edu.co>
2019-05-27 16:40:24 -0500

    Clase usuario con verficación de login

---
+++ mobile-app/AppLogin/app/src/main/java/com/example/applogin/DataAccess/Usuario.java
@@ -0,0 +1,41 @@
+package com.example.applogin.DataAccess;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class Usuario {
+
+    int identificacion;
+    int contrasena;
+
+    public Usuario(int identificacion, int contrasena){
+
+        this.identificacion = identificacion;
+        this.contrasena = contrasena;
+
+    }
+
+    public boolean verficarContrasena(Context context) {
+
+        Sqlite sqlite = new Sqlite(context, "administracion", null, 1);
+        SQLiteDatabase bd = sqlite.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select identificacion, claveNumerica from usuario where identificacion = " + identificacion + " and claveNumerica = " + contrasena, null);
+
+        if (fila.moveToFirst()) {
+
+            if (fila.getInt(1) == identificacion && fila.getInt(2) == contrasena) {
+
+                bd.close();
+                return true;
+
+            }
+
+        }
+
+        bd.close();
+        return false;
+
+    }
+
+}

404ec2485f2bdc59a08596df8122ddc3c33fa7d2
Gonzalo Diaz <egonzalodm@gmail.com>
2019-06-01 23:49:30 -0500

    CRud

--- mobile-app/AppLogin/app/src/main/java/com/example/applogin/DataAccess/Database/DataBase.java
+++ mobile-app/AppLogin/app/src/main/java/com/example/applogin/DataAccess/Database/DataBase.java
@@ -18,6 +18,10 @@ public class DataBase extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO user (id, name, phone, email, accountNumber, password)  VALUES (1013681625, 'Gonzalo Diaz', 3134340058,'gonzalo@gmail.com','1235-4654-3213-6546', 123456);");
         db.execSQL("create table admin(id integer primary key, name text, phone bigint, email text, password integer)");
         db.execSQL("INSERT INTO admin (id, name, phone, email, password)  VALUES (1013681625, 'Gonzalo Diaz', 3134340058,'gonzalo@gmail.com', 123456);");
+        db.execSQL("create table transactions(id integer primary key autoincrement, account1 text, account2 text, hour text, date text,type text, amount bigint)");
+        db.execSQL("INSERT INTO transactions (account1, account2, hour, date,type, amount)  VALUES ('1231-4141-1414-8787', '4568-9841-1223-1313', '10:30:26','2019-10-15', 100000);");
+        db.execSQL("create table account(account text primary key, amount bigint, state text)");
+        db.execSQL("INSERT INTO account (account, amount, state)  VALUES ('1231-4141-1414-8787', '100000', 'Aceptada');");
 
     }
 
@@ -28,7 +32,10 @@ public class DataBase extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO user (id, name, phone, email, accountNumber, password)  VALUES (1013681625, 'Gonzalo Diaz', 3134340058,'gonzalo@gmail.com','1235-4654-3213-6546', 123456);");
         db.execSQL("create table admin(id integer primary key, name text, phone bigint, email text, password integer)");
         db.execSQL("INSERT INTO admin (id, name, phone, email, password)  VALUES (1013681625, 'Gonzalo Diaz', 3134340058,'gonzalo@gmail.com', 123456);");
-
+        db.execSQL("create table transactions(id integer primary key autoincrement, account1 text, account2 text, hour text, date text,type text, amount bigint)");
+        db.execSQL("INSERT INTO transactions (account1, account2, hour, date,type, amount)  VALUES ('1231-4141-1414-8787', '4568-9841-1223-1313', '10:30:26','2019-10-15', 100000);");
+        db.execSQL("create table account(account text primary key, amount bigint, state text)");
+        db.execSQL("INSERT INTO account (account, amount, state)  VALUES ('1231-4141-1414-8787', '100000', 'Aceptada');");
     }
 
 }
